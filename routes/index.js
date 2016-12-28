1150c9ba4c00e42c3ea4e5803d588c8f9e863bed
Serjay Ilaga <serjay_ilaga@yahoo.com>
2016-12-28 19:05:30 +0800

    Major Modification -7

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+var name = 'fixIT';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+  var indexData = {
+  	title: name + ' - Your solution to IT Problems',
+	name: name,
+	description: 'Your solution to IT Problems.',
+	services: [
+        {
+            "name":"Hardware Repair",
+            "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a mollis est, sed dapibus neque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae",
+            "price_range":"PHP 1000 - PHP 2000",
+            "link": '/hardware',
+            "icon": 'fa.fa-wrench.large'
+        },
+        {
+            "name":"Pc Tune Up",
+            "description":"Adipiscing elit. Maecenas a mollis est, sed dapibus neque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae",
+            "price_range":"PHP 750 - PHP 1000",
+            "link": '/tuneup',
+            "icon": 'fa.fa-home.large'
+        }]
+};
+
+  res.render('index', indexData);
 });
 
 module.exports = router;
