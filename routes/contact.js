bb113f2e2d4f8b151c0d5178a17b979501f67296
Serjay Ilaga <serjay_ilaga@yahoo.com>
2017-02-01 17:03:48 +0800

    Fixed NodeMailer

--- routes/contact.js
+++ routes/contact.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var jadeCompiler = require('../lib/jadeCompiler');
 
 var nodemailer = require('nodemailer');
-
+/*
 var generator = require('xoauth2').createXOAuth2Generator({
     user: 'fixit.solution.v1@gmail.com',
     clientId: '917097726441-p35t6fo03goi9u5ct6k6n8rg0mv19fbc.apps.googleusercontent.com',
@@ -16,7 +16,7 @@ var generator = require('xoauth2').createXOAuth2Generator({
 generator.on('token', function(token){
     console.log('New token for %s: %s', token.user, token.accessToken);
 });
-
+*/
 
 var name = "";
 var email = "";
@@ -60,7 +60,13 @@ router.get('/', function(req, res, next) {
 var transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-        xoauth2: generator
+        type: 'OAuth2',
+        user: 'fixit.solution.v1@gmail.com',
+        clientId: '917097726441-p35t6fo03goi9u5ct6k6n8rg0mv19fbc.apps.googleusercontent.com',
+        clientSecret: 'bIIhfXrbunz6lwWJ-7lyYyEl',
+        refreshToken: '1/yhNux2J9zxZ6Tu-NmNzKJm_GSe_7zvf6uD5SPwvW9Xk',
+        accessToken: 'ya29.GlvlA8oq4Cmm-LR7KFGOAdB6bfn1S_vr7HkQnqpjXyV7H3QflTNjPcgG6QjiZXq3AC-c_wu67AcHCCIEa-mitEszJAkRJZASTpPBH-_4N2RHVFa4XFlGjltkRAxE'
+        //xoauth2: generator
     }
 });
 
